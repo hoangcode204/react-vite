1f196749367b52906ba81104cc022ecd2d8f6792
Pham Tuan <tuan.pv@ptf.com.vn>
2023-03-29 10:37:05 +0700

    video 41 - base preview detail book

--- src/components/Admin/Book/BookViewDetail.jsx
+++ src/components/Admin/Book/BookViewDetail.jsx
@@ -1,6 +1,9 @@
-import { Badge, Descriptions, Drawer } from "antd";
+import { Badge, Descriptions, Divider, Drawer, Modal, Upload } from "antd";
 import moment from 'moment';
 import { FORMAT_DATE_DISPLAY } from "../../../utils/constant";
+//FORMAT_DATE_DISPLAY = 'DD-MM-YYYY HH:mm:ss'
+import { useState } from "react";
+
 const BookViewDetail = (props) => {
     const { openViewDetail, setOpenViewDetail, dataViewDetail, setDataViewDetail } = props;
 
@@ -8,6 +11,63 @@ const BookViewDetail = (props) => {
         setOpenViewDetail(false);
         setDataViewDetail(null);
     }
+
+    const getBase64 = (file) =>
+        new Promise((resolve, reject) => {
+            const reader = new FileReader();
+            reader.readAsDataURL(file);
+            reader.onload = () => resolve(reader.result);
+            reader.onerror = (error) => reject(error);
+        });
+
+    const [previewOpen, setPreviewOpen] = useState(false);
+    const [previewImage, setPreviewImage] = useState('');
+    const [previewTitle, setPreviewTitle] = useState('');
+    const [fileList, setFileList] = useState([
+        {
+            uid: '-1',
+            name: 'image.png',
+            status: 'done',
+            url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+        },
+        {
+            uid: '-2',
+            name: 'image.png',
+            status: 'done',
+            url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+        },
+        {
+            uid: '-3',
+            name: 'image.png',
+            status: 'done',
+            url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+        },
+        {
+            uid: '-4',
+            name: 'image.png',
+            status: 'done',
+            url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+        }
+    ]);
+
+    const handleCancel = () => setPreviewOpen(false);
+
+    const handlePreview = async (file) => {
+        if (!file.url && !file.preview) {
+            file.preview = await getBase64(file.originFileObj);
+        }
+        setPreviewImage(file.url || (file.preview));
+        setPreviewOpen(true);
+        setPreviewTitle(file.name || file.url.substring(file.url.lastIndexOf('/') + 1));
+    };
+
+    const handleChange = ({ fileList: newFileList }) => {
+        setFileList(newFileList);
+    }
+
+
+
+
     return (
         <>
             <Drawer
@@ -17,18 +77,19 @@ const BookViewDetail = (props) => {
                 open={openViewDetail}
             >
                 <Descriptions
-                    title="Thông tin user"
+                    title="Thông tin Book"
                     bordered
                     column={2}
                 >
                     <Descriptions.Item label="Id">{dataViewDetail?._id}</Descriptions.Item>
-                    <Descriptions.Item label="Tên hiển thị">{dataViewDetail?.fullName}</Descriptions.Item>
-                    <Descriptions.Item label="Email">{dataViewDetail?.email}</Descriptions.Item>
-                    <Descriptions.Item label="Số điện thoại">{dataViewDetail?.phone}</Descriptions.Item>
+                    <Descriptions.Item label="Tên sách">{dataViewDetail?.mainText}</Descriptions.Item>
+                    <Descriptions.Item label="Tác giả">{dataViewDetail?.author}</Descriptions.Item>
+                    <Descriptions.Item label="Giá tiền">{new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(dataViewDetail?.price ?? 0)}</Descriptions.Item>
 
-                    <Descriptions.Item label="Role" span={2}>
-                        <Badge status="processing" text={dataViewDetail?.role} />
+                    <Descriptions.Item label="Thể loại" span={2}>
+                        <Badge status="processing" text={dataViewDetail?.category} />
                     </Descriptions.Item>
+
                     <Descriptions.Item label="Created At">
                         {moment(dataViewDetail?.createdAt).format(FORMAT_DATE_DISPLAY)}
                     </Descriptions.Item>
@@ -36,6 +97,22 @@ const BookViewDetail = (props) => {
                         {moment(dataViewDetail?.updatedAt).format(FORMAT_DATE_DISPLAY)}
                     </Descriptions.Item>
                 </Descriptions>
+                <Divider orientation="left" > Ảnh Books </Divider>
+                <Upload
+                    action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+                    listType="picture-card"
+                    fileList={fileList}
+                    onPreview={handlePreview}
+                    onChange={handleChange}
+                    showUploadList={
+                        { showRemoveIcon: false }
+                    }
+                >
+
+                </Upload>
+                <Modal open={previewOpen} title={previewTitle} footer={null} onCancel={handleCancel}>
+                    <img alt="example" style={{ width: '100%' }} src={previewImage} />
+                </Modal>
             </Drawer>
         </>
     )
